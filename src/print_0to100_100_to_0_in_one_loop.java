b02c992774dd7bcbb93cfb8bddfa9571b41d6de5
Arif <isarmybd@gmail.com>
2015-11-17 21:39:42 +0600

    print 0 -100 and 100-0 in one loop

---
+++ src/print_0to100_100_to_0_in_one_loop.java
@@ -0,0 +1,18 @@
+
+public class print_0to100_100_to_0_in_one_loop {
+	
+	public static void main(String args[]){
+		int i;
+		int j = 0;
+		for (i=1;i<=100;i++){
+			//System.out.print(" " + i);
+			j=100-i;
+			System.out.println(i+"      +    "+ "  "+ j +" = "+ (i+j));
+			
+			
+		}
+		
+		
+	}
+
+}
